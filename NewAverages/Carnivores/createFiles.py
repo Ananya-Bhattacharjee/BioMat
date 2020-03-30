367ecc496eca47ec39ca0ae149ea5232d73981e2
unknown <ananyab153258@gmail.com>
2020-03-30 14:29:58 +0600

    After review 2

---
+++ NewAverages/Carnivores/createFiles.py
@@ -0,0 +1,11 @@
+for ii in range(0, 10):
+    f = open("E:\\BioMat\\BioMat\\NewAverages\\Carnivores\\missing5\\missing5" + str(ii) + "Dambe.DND", "w+")
+    ff = open("E:\\BioMat\\BioMat\\NewAverages\\Carnivores\\missing5\\missing5" + str(ii) + "Matfact.DND", "w+")
+    fff = open("E:\\BioMat\\BioMat\\NewAverages\\Carnivores\\missing5\\missing5" + str(ii) + "Encoder.DND", "w+")
+    f4 = open("E:\\BioMat\\BioMat\\NewAverages\\Carnivores\\missing5\\missing5" + str(ii) + ".dis", "w+")
+
+
+    f.close
+    ff.close
+    fff.close
+    f4.close
